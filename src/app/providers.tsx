d30d6790e2e5ce1ce17482ea88001c1a32d45c60
Deezor1 <skunks400@gmail.com>
2024-02-04 16:44:01 +0100

    adding suspense

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,10 +1,11 @@
 'use client';
 import React from 'react';
 import { Next13ProgressBar } from 'next13-progressbar';
+import { Suspense } from 'react';
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
-    <>
+    <Suspense>
       {children}
       <Next13ProgressBar
         height='2px'
@@ -12,7 +13,7 @@ const Providers = ({ children }: { children: React.ReactNode }) => {
         options={{ showSpinner: false }}
         showOnShallow
       />
-    </>
+    </Suspense>
   );
 };
 
